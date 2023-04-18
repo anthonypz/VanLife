ee86a26ff6ba60346a01a78854f717bb9b657d9f
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2023-04-17 17:40:44 -0700

    Cleanup nested a tags

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,63 +9,61 @@ export default function Header() {
     color: "#161616",
   }
 
-  function fakeLogOut() {
-    localStorage.removeItem("loggedin")
-  }
-
   return (
     <header>
       <Navbar
         fluid={true}
         rounded={true}
       >
-        <Navbar.Brand>
-          <Link
-            className="self-center whitespace-nowrap text-2xl font-black uppercase dark:text-white hover:underline"
-            to="/"
-          >
-            #VanLife
-          </Link>
-        </Navbar.Brand>
+        {/* <Navbar.Brand> */}
+        <Link
+          className="self-center whitespace-nowrap text-2xl font-black uppercase dark:text-white hover:underline"
+          to="/"
+        >
+          #VanLife
+        </Link>
+        {/* </Navbar.Brand> */}
         <Navbar.Toggle />
         <Navbar.Collapse>
-          <Navbar.Link>
-            <NavLink
-              className="text-slate-600 font-semibold hover:text-slate-800 hover:underline text-base"
-              to="host"
-              style={({ isActive }) => (isActive ? activeStyles : null)}
-            >
-              Host
-            </NavLink>
-          </Navbar.Link>
-          <Navbar.Link>
-            <NavLink
-              className="text-slate-600 font-semibold hover:text-slate-800 hover:underline text-base"
-              to="about"
-              style={({ isActive }) => (isActive ? activeStyles : null)}
-            >
-              About
-            </NavLink>
-          </Navbar.Link>
-          <Navbar.Link>
-            <NavLink
-              className="text-slate-600 font-semibold hover:text-slate-800 hover:underline text-base"
-              to="vans"
-              style={({ isActive }) => (isActive ? activeStyles : null)}
-            >
-              Vans
-            </NavLink>
-          </Navbar.Link>
-          <Navbar.Link>
-            <Link to="login">
-              <img
-                src={loginIcon}
-                className="w-[20px] h-auto mt-[2px]"
-                alt="login icon"
-              />
-            </Link>
-          </Navbar.Link>
-          <button onClick={fakeLogOut}>X</button>
+          {/* <Navbar.Link> */}
+          <NavLink
+            className="text-slate-600 font-semibold hover:text-slate-800 hover:underline text-base md:border-0 md:hover:bg-transparent block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 hover:bg-gray-50"
+            to="host"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Host
+          </NavLink>
+          {/* </Navbar.Link> */}
+          {/* <Navbar.Link> */}
+          <NavLink
+            className="text-slate-600 font-semibold hover:text-slate-800 hover:underline text-base md:border-0 md:hover:bg-transparent block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 hover:bg-gray-50"
+            to="about"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            About
+          </NavLink>
+          {/* </Navbar.Link> */}
+          {/* <Navbar.Link> */}
+          <NavLink
+            className="text-slate-600 font-semibold hover:text-slate-800 hover:underline text-base md:border-0 md:hover:bg-transparent block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 hover:bg-gray-50"
+            to="vans"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Vans
+          </NavLink>
+          {/* </Navbar.Link> */}
+          {/* <Navbar.Link> */}
+          <Link
+            className="text-slate-600 font-semibold hover:text-slate-800 hover:underline text-base md:border-0 md:hover:bg-transparent block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 hover:bg-gray-50"
+            to="login"
+          >
+            <img
+              src={loginIcon}
+              className="w-[20px] h-auto mt-[2px]"
+              alt="login icon"
+            />
+          </Link>
+          {/* </Navbar.Link> */}
         </Navbar.Collapse>
       </Navbar>
     </header>
