f2b7eb6947f73cf38a6e2c44f1960f32ce37433c
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2023-04-18 17:52:04 -0700

    Remove unused User schema code Handle User data directly from Clerk instead.

--- app/routes/sign-up.$.tsx
+++ app/routes/sign-up.$.tsx
@@ -1,15 +1,4 @@
 import { SignUp } from "@clerk/remix"
-import type { ActionArgs } from "@remix-run/node"
-import { createUser } from "~/models/user.server"
-import { getAuth } from "@clerk/remix/ssr.server"
-
-export async function action(args: ActionArgs) {
-  console.log("ran the action!")
-  const { userId } = await getAuth(args)
-  if (userId) {
-    await createUser(userId)
-  }
-}
 
 export default function SignUpPage() {
   return (
