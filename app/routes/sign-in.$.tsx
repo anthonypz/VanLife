e450d5f1f01bd772a9c299f1a979c9dcec4c58ec
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2023-04-22 01:02:04 -0700

    Redirect user to intended path after signing in

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -1,11 +1,21 @@
 import { SignIn } from "@clerk/remix"
+import type { LoaderArgs } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+
+export function loader({ request }: LoaderArgs) {
+  const pathname =
+    new URL(request.url).searchParams.get("redirectTo") || "/host"
+  return pathname
+}
 
 export default function SignInPage() {
+  const path = useLoaderData<typeof loader>()
   return (
     <div className="flex justify-center items-center py-8 px-6 md:h-screen">
       <SignIn
         routing={"path"}
         path={"/sign-in"}
+        redirectUrl={path}
       />
     </div>
   )
