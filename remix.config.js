743f66a24e7f921ca81ea2b972df951a292c883e
Anthony P <43166263+anthonypz@users.noreply.github.com>
2023-06-16 22:37:46 -0700

    Update tailwind config option

--- remix.config.js
+++ remix.config.js
@@ -1,19 +1,19 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ['**/.*'],
   server:
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
-      ? "./server.js"
+      ? './server.js'
       : undefined,
-  serverBuildPath: ".netlify/functions-internal/server.js",
+  serverBuildPath: '.netlify/functions-internal/server.js',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
+  tailwind: true,
   future: {
-    unstable_tailwind: true,
-    // v2_errorBoundary: true,
+    v2_errorBoundary: false,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
-}
+};
