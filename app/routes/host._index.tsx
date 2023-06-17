40c35bf11530db20bd318e8dce598b10598df234
Anthony P <43166263+anthonypz@users.noreply.github.com>
2023-06-16 22:41:58 -0700

    Add error element to Await component

--- app/routes/host._index.tsx
+++ app/routes/host._index.tsx
@@ -1,135 +1,140 @@
-import React from "react"
-import { Link, Await, useLoaderData } from "@remix-run/react"
-import { defer } from "@remix-run/node"
-import type { LoaderArgs } from "@remix-run/node"
-import type { Van } from "~/models/van.server"
-import { getHostVans } from "~/models/van.server"
-import { Rating } from "flowbite-react"
-import { getUsername, requireAuth } from "~/utils.server"
+import React from 'react';
+import { Link, Await, useLoaderData } from '@remix-run/react';
+import { defer } from '@remix-run/node';
+import type { LoaderArgs } from '@remix-run/node';
+import type { Van } from '~/models/van.server';
+import { getHostVans } from '~/models/van.server';
+import { Rating } from 'flowbite-react';
+import { getUsername, requireAuth } from '~/utils.server';
 
 export async function loader(args: LoaderArgs) {
-  const userId = await requireAuth(args)
-  const username = await getUsername(userId)
-  return defer({ vans: getHostVans(userId), username })
+  const userId = await requireAuth(args);
+  const username = await getUsername(userId);
+  return defer({ vans: getHostVans(userId), username });
 }
 
-type VanElements = Omit<Van, "createdAt" | "updatedAt">
+type VanElements = Omit<Van, 'createdAt' | 'updatedAt'>;
 
 export default function Dashboard() {
-  const loaderData = useLoaderData<typeof loader>()
+  const loaderData = useLoaderData<typeof loader>();
 
   function renderVanElements(vans: VanElements[]) {
-    const hostVansEls = vans.map((van) => (
+    const hostVansEls = vans?.map((van) => (
       <div
-        className="flex items-center bg-white mb-4 py-4 px-6 border border-orange-200 rounded"
+        className='flex items-center bg-white mb-4 py-4 px-6 border border-orange-200 rounded'
         key={van.id}
       >
         <img
           src={van.imageUrl}
           alt={`${van.name}`}
-          className="rounded h-20 mr-4"
+          className='rounded h-20 mr-4'
         />
-        <div className="mr-auto">
-          <h3 className="text-xl font-semibold my-2">{van.name}</h3>
-          <p className="">${van.price}/day</p>
+        <div className='mr-auto'>
+          <h3 className='text-xl font-semibold my-2'>{van.name}</h3>
+          <p className=''>${van.price}/day</p>
         </div>
         <Link
           to={`vans/${van.id}`}
-          className="p-2"
+          className='p-2'
         >
           View
         </Link>
       </div>
-    ))
+    ));
 
     return (
-      <div className="flex flex-col">
+      <div className='flex flex-col'>
         <section>{hostVansEls}</section>
         <Link
-          to="new"
-          className="self-end mt-2 text-white bg-[#ff8c38] hover:bg-[#ff8c38]/80 focus:ring-4 focus:outline-none focus:ring-[#ff8c38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  dark:hover:bg-[#ff8c38]/80 dark:focus:ring-[#ff8c38]/40"
+          to='new'
+          className='self-end mt-2 text-white bg-[#ff8c38] hover:bg-[#ff8c38]/80 focus:ring-4 focus:outline-none focus:ring-[#ff8c38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  dark:hover:bg-[#ff8c38]/80 dark:focus:ring-[#ff8c38]/40'
         >
           Add a van
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
             strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 ml-2 -mr-1"
+            stroke='currentColor'
+            className='w-6 h-6 ml-2 -mr-1'
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
             />
           </svg>
         </Link>
       </div>
-    )
+    );
   }
 
   return (
     <>
-      <section className="flex justify-between items-center flex-wrap px-2 py-8 sm:p-8 bg-[#ffead0]">
-        <div className="">
-          <h1 className="text-4xl font-bold">
+      <section className='flex justify-between items-center flex-wrap px-2 py-8 sm:p-8 bg-[#ffead0]'>
+        <div className=''>
+          <h1 className='text-4xl font-bold'>
             {loaderData.username
               ? `Welcome, ${loaderData.username}`
-              : "Welcome"}
+              : 'Welcome'}
             !
           </h1>
-          <p className="text-neutral-700 my-4">
-            Income last{" "}
-            <span className="text-neutral-700 font-bold underline">
+          <p className='text-neutral-700 my-4'>
+            Income last{' '}
+            <span className='text-neutral-700 font-bold underline'>
               30 days
             </span>
           </p>
-          <h2 className="text-4xl font-black">$2,260</h2>
+          <h2 className='text-4xl font-black'>$2,260</h2>
         </div>
         <Link
-          to="income"
-          className="p-2 hover:underline"
+          to='income'
+          className='p-2 hover:underline'
         >
           Details
         </Link>
       </section>
-      <section className="flex flex-wrap items-center px-2 py-8 sm:p-8 bg-[#ffddb2]">
-        <h2 className="text-2xl font-bold">Review score</h2>
-        <Rating className="mr-auto">
-          <Rating.Star className="ml-3 w-8 h-8" />
-          <p className="ml-2 text-xl font-bold text-gray-900 dark:text-white">
+      <section className='flex flex-wrap items-center px-2 py-8 sm:p-8 bg-[#ffddb2]'>
+        <h2 className='text-2xl font-bold'>Review score</h2>
+        <Rating className='mr-auto'>
+          <Rating.Star className='ml-3 w-8 h-8' />
+          <p className='ml-2 text-xl font-bold text-gray-900 dark:text-white'>
             5
           </p>
-          <span className="mx-1.5 h-1 w-1 rounded-full bg-gray-500 dark:bg-gray-400" />
+          <span className='mx-1.5 h-1 w-1 rounded-full bg-gray-500 dark:bg-gray-400' />
           <Link
-            to="reviews"
-            className="text-base font-medium text-gray-900 underline hover:no-underline dark:text-white"
+            to='reviews'
+            className='text-base font-medium text-gray-900 underline hover:no-underline dark:text-white'
           >
             2 reviews
           </Link>
         </Rating>
         <Link
-          to="reviews"
-          className="p-2 hover:underline"
+          to='reviews'
+          className='p-2 hover:underline'
         >
           Details
         </Link>
       </section>
-      <section className="py-8 px-0 sm:p-8">
-        <div className="flex justify-between items-center">
-          <h2 className="text-2xl font-bold my-4">Your listed vans</h2>
+      <section className='py-8 px-0 sm:p-8'>
+        <div className='flex justify-between items-center'>
+          <h2 className='text-2xl font-bold my-4'>Your listed vans</h2>
           <Link
-            to="vans"
-            className="p-2 hover:underline"
+            to='vans'
+            className='p-2 hover:underline'
           >
             View all
           </Link>
         </div>
         <React.Suspense fallback={<h3>Loading...</h3>}>
-          <Await resolve={loaderData.vans}>{renderVanElements}</Await>
+          <Await
+            resolve={loaderData.vans}
+            errorElement={<p className='text-red-500'>Error loading vans</p>}
+          >
+            {renderVanElements}
+          </Await>
         </React.Suspense>
       </section>
     </>
-  )
+  );
 }
--- app/routes/vans.tsx
+++ app/routes/vans.tsx
@@ -1,41 +1,41 @@
-import React from "react"
-import { Link, useSearchParams, useLoaderData, Await } from "@remix-run/react"
-import { defer } from "@remix-run/node"
-import { getAllVans } from "~/models/van.server"
-import type { Van } from "~/models/van.server"
+import React from 'react';
+import { Link, useSearchParams, useLoaderData, Await } from '@remix-run/react';
+import { defer } from '@remix-run/node';
+import { getAllVans } from '~/models/van.server';
+import type { Van } from '~/models/van.server';
 
 export function loader() {
-  return defer({ vans: getAllVans() })
+  return defer({ vans: getAllVans() });
 }
 
-type VanElements = Omit<Van, "createdAt" | "updatedAt">
+type VanElements = Omit<Van, 'createdAt' | 'updatedAt'>;
 
 export default function Vans() {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const dataPromise = useLoaderData<typeof loader>()
+  const [searchParams, setSearchParams] = useSearchParams();
+  const dataPromise = useLoaderData<typeof loader>();
 
-  const typeFilter = searchParams.get("type")
+  const typeFilter = searchParams.get('type');
 
   function handleFilterChange(key: string, value: string | null) {
     setSearchParams((prevParams) => {
       if (value === null) {
-        prevParams.delete(key)
+        prevParams.delete(key);
       } else {
-        prevParams.set(key, value)
+        prevParams.set(key, value);
       }
-      return prevParams
-    })
+      return prevParams;
+    });
   }
 
   function renderVanElements(vans: VanElements[]) {
     const displayedVans = typeFilter
       ? vans.filter((van) => van.type === typeFilter)
-      : vans
+      : vans;
 
-    const vanElements = displayedVans.map((van) => (
+    const vanElements = displayedVans?.map((van) => (
       <div
         key={van.id}
-        className="bg-cover bg-center"
+        className='bg-cover bg-center'
       >
         <Link
           to={van.id}
@@ -46,12 +46,12 @@ export default function Vans() {
         >
           <img
             src={van.imageUrl}
-            alt="travel van"
-            className="rounded h-auto max-w-full"
+            alt='travel van'
+            className='rounded h-auto max-w-full'
           />
-          <div className="mb-3">
-            <h3 className="font-bold my-2 text-lg text-gray-900">{van.name}</h3>
-            <p className="text-base text-gray-900">
+          <div className='mb-3'>
+            <h3 className='font-bold my-2 text-lg text-gray-900'>{van.name}</h3>
+            <p className='text-base text-gray-900'>
               ${van.price}
               <span>/day</span>
             </p>
@@ -63,54 +63,59 @@ export default function Vans() {
           </button>
         </Link>
       </div>
-    ))
+    ));
 
     return (
       <>
-        <div className="flex flex-wrap gap-1">
+        <div className='flex flex-wrap gap-1'>
           <button
-            onClick={() => handleFilterChange("type", "simple")}
-            className="py-2 px-7 rounded bg-[#ffead0] text-gray-700 mr-5 hover:bg-[#e17654] hover:text-[#ffead0] focus:bg-[#e17654] focus:text-[#ffead0] font-medium"
+            onClick={() => handleFilterChange('type', 'simple')}
+            className='py-2 px-7 rounded bg-[#ffead0] text-gray-700 mr-5 hover:bg-[#e17654] hover:text-[#ffead0] focus:bg-[#e17654] focus:text-[#ffead0] font-medium'
           >
             Simple
           </button>
           <button
-            onClick={() => handleFilterChange("type", "luxury")}
-            className="py-2 px-7 rounded bg-[#ffead0] text-gray-700 mr-5 hover:bg-[#161616] hover:text-[#ffead0] focus:bg-[#161616] focus:text-[#ffead0] font-medium"
+            onClick={() => handleFilterChange('type', 'luxury')}
+            className='py-2 px-7 rounded bg-[#ffead0] text-gray-700 mr-5 hover:bg-[#161616] hover:text-[#ffead0] focus:bg-[#161616] focus:text-[#ffead0] font-medium'
           >
             Luxury
           </button>
           <button
-            onClick={() => handleFilterChange("type", "rugged")}
-            className="py-2 px-7 rounded bg-[#ffead0] text-gray-700 mr-5 hover:bg-[#115e59] hover:text-[#ffead0] focus:bg-[#115e59] focus:text-[#ffead0] font-medium"
+            onClick={() => handleFilterChange('type', 'rugged')}
+            className='py-2 px-7 rounded bg-[#ffead0] text-gray-700 mr-5 hover:bg-[#115e59] hover:text-[#ffead0] focus:bg-[#115e59] focus:text-[#ffead0] font-medium'
           >
             Rugged
           </button>
 
           {typeFilter ? (
             <button
-              onClick={() => handleFilterChange("type", null)}
-              className="py-2 px-7 pl-0 rounded text-gray-600 underline mr-5 font-medium"
+              onClick={() => handleFilterChange('type', null)}
+              className='py-2 px-7 pl-0 rounded text-gray-600 underline mr-5 font-medium'
             >
               Clear filter
             </button>
           ) : null}
         </div>
-        <div className="mt-12 grid grid-cols-2 justify-items-center gap-8">
+        <div className='mt-12 grid grid-cols-2 justify-items-center gap-8'>
           {vanElements}
         </div>
       </>
-    )
+    );
   }
 
   return (
-    <div className="container mx-auto px-2 sm:px-6 py-8">
-      <h1 className="text-3xl text-gray-900 font-bold my-5">
+    <div className='container mx-auto px-2 sm:px-6 py-8'>
+      <h1 className='text-3xl text-gray-900 font-bold my-5'>
         Explore our van options
       </h1>
       <React.Suspense fallback={<h2>Loading vans...</h2>}>
-        <Await resolve={dataPromise.vans}>{renderVanElements}</Await>
+        <Await
+          resolve={dataPromise.vans}
+          errorElement={<p className='text-red-500'>Error loading vans</p>}
+        >
+          {renderVanElements}
+        </Await>
       </React.Suspense>
     </div>
-  )
+  );
 }
